b0e9a64a89d1030cd2b6a228579b1e74055fc282
Melvin Tan <melvintan123@gmail.com>
2018-02-04 23:51:59 +0800

    modified Address class to include block, street, unit, postal code fields.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -2,6 +2,8 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+import java.util.ArrayList;
+
 /**
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
@@ -12,7 +14,12 @@ public class Address {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
+    private final String _block;
+    private final String _street;
+    private final String _unit;
+    private final String _postalCode;
+    public final String value; //not a field in the LO-ImplementClass, but without, needs lots of modifications
+
     private boolean isPrivate;
 
     /**
@@ -26,8 +33,36 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+
+        String[] splitAddress = splitAddress(trimmedAddress);
+
+        _block = splitAddress[0];
+        _street = splitAddress[1];
+        _unit = splitAddress[2];
+        _postalCode = splitAddress[3];
+        value = _block + " " + _street + " " + _unit + " " + _postalCode;
     }
+
+    /**
+     * splits a given address into four Strings representing block, street, unit, postal code
+     * @param  address input by user
+     * @return split address
+     */
+    private String[] splitAddress(String address) {
+        String[] splitAddress = new String[4];
+
+        //find indices of the 3 ','.
+        String tempStr = address;
+        for (int i = 0; i < 3; i++) {
+            int commaIndex = tempStr.indexOf(',');
+            splitAddress[i] = tempStr.substring(0, commaIndex + 1);
+            tempStr = tempStr.substring(commaIndex + 2);
+        }
+        splitAddress[3] = tempStr;
+
+        return splitAddress;
+    }
+
 
     /**
      * Returns true if a given string is a valid person address.
@@ -38,19 +73,19 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return _block + " " + _street + " " + _unit + " " +_postalCode + ".";
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this._postalCode.equals(((Address) other)._postalCode)); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return _postalCode.hashCode();
     }
 
     public boolean isPrivate() {
