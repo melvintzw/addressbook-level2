17a2e5c1b7e02e92cc05f722968e1f5be7b99e10
Melvin Tan <melvintan123@gmail.com>
2018-02-05 16:41:40 +0800

    addded classes for block, street, unit, postal code under Address class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -14,11 +14,19 @@ public class Address {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
+    /*
     private final String _block;
     private final String _street;
     private final String _unit;
     private final String _postalCode;
+    */
+
     public final String value; //not a field in the LO-ImplementClass, but without, needs lots of modifications
+
+    private final Block block = new Block();
+    private final Street street = new Street();
+    private final Unit unit = new Unit();
+    private final PostalCode postalCode = new PostalCode();
 
     private boolean isPrivate;
 
@@ -36,22 +44,32 @@ public class Address {
 
         String[] splitAddress = splitAddress(trimmedAddress);
 
+        /*
         _block = splitAddress[0];
         _street = splitAddress[1];
         _unit = splitAddress[2];
         _postalCode = splitAddress[3];
         value = _block + " " + _street + " " + _unit + " " + _postalCode;
+        */
+
+        block.setData(splitAddress[0]);
+        street.setData(splitAddress[1]);
+        unit.setData(splitAddress[2]);
+        postalCode.setData(splitAddress[3]);
+
+        value = block.getData() + " " + street.getData() + " " + unit.getData() + " " + postalCode.getData();
     }
 
     /**
      * splits a given address into four Strings representing block, street, unit, postal code
+     * first 3 fields contains the comma delimiter
      * @param  address input by user
      * @return split address
      */
     private String[] splitAddress(String address) {
         String[] splitAddress = new String[4];
 
-        //find indices of the 3 ','.
+        //find indices of the commas e.g. "123, Clementi Avenue 3, #
         String tempStr = address;
         for (int i = 0; i < 3; i++) {
             int commaIndex = tempStr.indexOf(',');
@@ -71,21 +89,73 @@ public class Address {
         return test.matches(ADDRESS_VALIDATION_REGEX);
     }
 
+    class Block {
+        private String _data;
+
+        public String getData(){
+            return _data;
+        }
+
+        public void setData(String data) {
+            _data = data;
+        }
+
+    }
+
+    class Street {
+        private String _data;
+
+        public String getData(){
+            return _data;
+        }
+
+        public void setData(String data) {
+            _data = data;
+        }
+
+    }
+
+    class Unit {
+        private String _data;
+
+        public String getData(){
+            return _data;
+        }
+
+        public void setData(String data) {
+            _data = data;
+        }
+
+    }
+
+    class PostalCode {
+        private String _data;
+
+        public String getData(){
+            return _data;
+        }
+
+        public void setData(String data) {
+            _data = data;
+        }
+
+    }
+
     @Override
     public String toString() {
-        return _block + " " + _street + " " + _unit + " " +_postalCode + ".";
+        return value;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this._postalCode.equals(((Address) other)._postalCode)); // state check
+                && this.value.equals(((Address) other).value)); // state check
     }
 
     @Override
     public int hashCode() {
-        return _postalCode.hashCode();
+        return value.hashCode();
     }
 
     public boolean isPrivate() {
